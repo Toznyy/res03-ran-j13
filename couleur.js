8108f1d0dd430bf1e1112e8c34fdba6cea249a30
Toznyy <anthony.cormier29860@gmail.com>
2022-11-24 15:14:12 +0000

    ajout du js

--- couleur.js
+++ couleur.js
@@ -5,37 +5,52 @@ window.addEventListener("DOMContentLoaded", function(event){
 function getNextSection(current){
     if (current.id === "red"){
         return document.getElementById("orange");}
-    if (current.id === "orange"){
+    else if (current.id === "orange"){
         return document.getElementById("yellow");}
-    if (current.id === "yellow"){
+    else if (current.id === "yellow"){
         return document.getElementById("green");}
-    if (current.id === "green"){
+    else if (current.id === "green"){
         return document.getElementById("blue");}
-    if (current.id === "blue"){
+    else if (current.id === "blue"){
         return document.getElementById("purple");}
+    else if (current.id === "purple"){
+        return undefined;}
 }
 
 function getPreviousSection(current){
     if (current.id === "purple"){
         return document.getElementById("blue");}
-    if (current.id === "blue"){
+    else if (current.id === "blue"){
         return document.getElementById("green");}
-    if (current.id === "green"){
+    else if (current.id === "green"){
         return document.getElementById("yellow");}
-    if (current.id === "yellow"){
+    else if (current.id === "yellow"){
         return document.getElementById("orange");}
-    if (current.id === "orange"){
+    else if (current.id === "orange"){
         return document.getElementById("red");}
+    else if (current.id === "red"){
+        return undefined;}
 }
 
 let sections = document.querySelectorAll("body > main > section");
+
 for (let i = 0; i < sections.length; i++){
-    document.addEventListener("wheel", function(event){
-        if(event.deltaY > 0){
-            getNextSection(sections[i]);}
-        else if(event.deltaY < 0){
-            getPreviousSection(sections[i]);}
-        let position = sections[i].getBoundingClientRect().top;
-        console.log(position)
+
+    
+    sections[i].addEventListener("wheel", function(event){
+        
+        let currentScrollPosition = document.documentElement.scrollTop;
+        
+        if(event.deltaY > 0 && getNextSection(sections[i]) != undefined){
+            
+            let nextPosition = getNextSection(sections[i]).getBoundingClientRect().top;
+            
+            scroll(0, nextPosition + currentScrollPosition);}
+            
+        else if(event.deltaY < 0 && getPreviousSection(sections[i]) != undefined){
+            
+            let previousPosition = getPreviousSection(sections[i]).getBoundingClientRect().top;
+            
+            scroll(0, previousPosition + currentScrollPosition);}
     });
 }
