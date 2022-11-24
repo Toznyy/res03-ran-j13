5b669bb67e1b80ccfa94724e2e1dbf314b8cb229
Toznyy <anthony.cormier29860@gmail.com>
2022-11-24 14:00:29 +0000

    ajout

--- couleur.js
+++ couleur.js
@@ -4,28 +4,38 @@ window.addEventListener("DOMContentLoaded", function(event){
 
 function getNextSection(current){
     if (current.id === "red"){
-        return document.getElementById("orange")}
+        return document.getElementById("orange");}
     if (current.id === "orange"){
-        return document.getElementById("yellow")}
+        return document.getElementById("yellow");}
     if (current.id === "yellow"){
-        return document.getElementById("green")}
+        return document.getElementById("green");}
     if (current.id === "green"){
-        return document.getElementById("blue")}
+        return document.getElementById("blue");}
     if (current.id === "blue"){
-        return document.getElementById("purple")}
+        return document.getElementById("purple");}
 }
 
-function getPreviousSec(current){
+function getPreviousSection(current){
     if (current.id === "purple"){
-        return document.getElementById("blue")}
+        return document.getElementById("blue");}
     if (current.id === "blue"){
-        return document.getElementById("green")}
+        return document.getElementById("green");}
     if (current.id === "green"){
-        return document.getElementById("yellow")}
+        return document.getElementById("yellow");}
     if (current.id === "yellow"){
-        return document.getElementById("orange")}
+        return document.getElementById("orange");}
     if (current.id === "orange"){
-        return document.getElementById("red")}
+        return document.getElementById("red");}
 }
 
 let sections = document.querySelectorAll("body > main > section");
+for (let i = 0; i < sections.length; i++){
+    document.addEventListener("wheel", function(event){
+        if(event.deltaY > 0){
+            getNextSection(sections[i]);}
+        else if(event.deltaY < 0){
+            getPreviousSection(sections[i]);}
+        let position = sections[i].getBoundingClientRect().top;
+        console.log(position)
+    });
+}
