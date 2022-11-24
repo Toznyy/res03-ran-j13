eff3bebcb329a05f99ac2dcbcc2886bd0b985186
Toznyy <anthony.cormier29860@gmail.com>
2022-11-24 11:30:42 +0000

    ajout

--- couleur.js
+++ couleur.js
@@ -1,3 +1,31 @@
 window.addEventListener("DOMContentLoaded", function(event){
 
 });
+
+function getNextSection(current){
+    if (current.id === "red"){
+        return document.getElementById("orange")}
+    if (current.id === "orange"){
+        return document.getElementById("yellow")}
+    if (current.id === "yellow"){
+        return document.getElementById("green")}
+    if (current.id === "green"){
+        return document.getElementById("blue")}
+    if (current.id === "blue"){
+        return document.getElementById("purple")}
+}
+
+function getPreviousSec(current){
+    if (current.id === "purple"){
+        return document.getElementById("blue")}
+    if (current.id === "blue"){
+        return document.getElementById("green")}
+    if (current.id === "green"){
+        return document.getElementById("yellow")}
+    if (current.id === "yellow"){
+        return document.getElementById("orange")}
+    if (current.id === "orange"){
+        return document.getElementById("red")}
+}
+
+let sections = document.querySelectorAll("body > main > section");
